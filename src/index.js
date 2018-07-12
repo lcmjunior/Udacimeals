1225a53ec1a56f71346efec9237adc2f6a2ab3a6
Luiz Claudio Medina Junior <lcmedinajunior@outlook.com>
2018-07-12 17:54:28 -0300

    Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./componentes/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
